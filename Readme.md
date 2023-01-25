67605ec51833d90242acf8ecf9b6cb5b95f9b923
zin-htun <zinlinhtun34@gmail.com>
2023-01-25 17:58:39 +0000

    First Dev Commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+## Test Project for DevOps Module
+- will be updated every change
+- will test around with classes and stuff
+- will be used for the final `implementation` if member agreed.  
---
+++ src/main/java/TestMain.java
@@ -0,0 +1,12 @@
+
+// Author : zin-lin
+// Purpose : Test
+// Organisation : Napier DevOps 13
+public class TestMain {
+
+    // Test Class main
+    public static void main (String [] args){
+        System.out.println("Welcome");
+    }
+
+}
